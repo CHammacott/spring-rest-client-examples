5edb2d2a86414ff25ed2d791155d2d51fe4380c3
Christian Hammacott <chammacott@tier2consulting.com>
2022-01-26 14:37:15 +0000

    Final

--- src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
+++ src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
@@ -1,5 +1,6 @@
 package guru.springframework.springrestclientexamples.controllers;
 
+import guru.springframework.api.domain.User;
 import guru.springframework.springrestclientexamples.services.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -27,12 +30,18 @@ public class UserController {
 
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange){
-
         serverWebExchange.getFormData()
                 .map(map -> map.get("limit").get(0))
                 .map(Integer::parseInt)
                 .map(apiService::getUsers)
                 .subscribe(data -> model.addAttribute("users", data));
         return "userList";
+
+        //todo Blows up due to url issue and not parsing json
+//        model.addAttribute("users",
+//                apiService.getUsers(serverWebExchange
+//                        .getFormData()
+//                        .map(data -> new Integer(data.getFirst("limit")))));
+//        return "userlist";
     }
 }
--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,9 +2,16 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -13,19 +20,36 @@ public class ApiServiceImpl implements  ApiService {
 
     private RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        List<LinkedHashMap<String, Object>> apiData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+        String fullApiUrl = api_url + "limit=" + limit;
+
+        List<LinkedHashMap<String, Object>> apiData = restTemplate.getForObject(fullApiUrl, List.class);
 
         UserData userData = new UserData();
 
         userData.ingestApiUsers(apiData);
 
         return userData.getData();
+    }
+
+    //todo make it work? Nah BROKEN
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient.create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", String.valueOf(limit.block())).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(resp -> resp.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 }
